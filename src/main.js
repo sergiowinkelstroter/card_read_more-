b6a3df692cf3a60d9c1c5e2a8d098e6cbac7f58f
Sergio <winksousa0@gmail.com>
2022-12-22 09:49:27 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+const button = document.getElementById("read_button");
+
+button.addEventListener("click", () => {
+  const card = document.querySelector(".card");
+
+  card.classList.toggle("active");
+
+  if (card.classList.contains("active")) {
+    return (button.textContent = "Read less");
+  }
+
+  button.textContent = "Read more";
+});
